93719a61d74ae47b4c4d5d6ec1f1470c41040b54
wangxingkang <wang_xingkang@qq.com>
2022-03-28 23:43:12 +0800

    ✨ feat: add icons

---
+++ packages/react/src/icons/AlignRightOutlined.tsx
@@ -0,0 +1,12 @@
+// This icon file is generated automatically.
+import * as React from 'react'
+import AlignRightOutlinedSvg from '@sensoro-design/icons-svg/lib/asn/AlignRightOutlined';
+import SensoroIcon, { SensoroIconProps } from '../components/SensoroIcon';
+
+const AlignRightOutlined = React.forwardRef<HTMLSpanElement, SensoroIconProps>(
+  (props, ref) => <SensoroIcon {...props} ref={ref} icon={AlignRightOutlinedSvg} />
+);
+
+AlignRightOutlined.displayName = 'AlignRightOutlined';
+
+export default AlignRightOutlined;
