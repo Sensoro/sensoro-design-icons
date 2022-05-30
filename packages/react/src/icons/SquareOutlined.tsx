a45e01bc63a2b5fae1832c664829c4f6d9dc7471
wangxingkang <wang_xingkang@qq.com>
2022-05-30 09:24:45 +0800

    chore: add icons

---
+++ packages/react/src/icons/SquareOutlined.tsx
@@ -0,0 +1,12 @@
+// This icon file is generated automatically.
+import * as React from 'react';
+import SquareOutlinedSvg from '@sensoro-design/icons-svg/lib/asn/SquareOutlined';
+import SensoroIcon, { SensoroIconProps } from '../components/SensoroIcon';
+
+const SquareOutlined = React.forwardRef<HTMLSpanElement, SensoroIconProps>((props, ref) => (
+  <SensoroIcon {...props} ref={ref} icon={SquareOutlinedSvg} />
+));
+
+SquareOutlined.displayName = 'SquareOutlined';
+
+export default SquareOutlined;
---
+++ packages/react/src/icons/UploadFilled.tsx
@@ -0,0 +1,12 @@
+// This icon file is generated automatically.
+import * as React from 'react';
+import UploadFilledSvg from '@sensoro-design/icons-svg/lib/asn/UploadFilled';
+import SensoroIcon, { SensoroIconProps } from '../components/SensoroIcon';
+
+const UploadFilled = React.forwardRef<HTMLSpanElement, SensoroIconProps>((props, ref) => (
+  <SensoroIcon {...props} ref={ref} icon={UploadFilledSvg} />
+));
+
+UploadFilled.displayName = 'UploadFilled';
+
+export default UploadFilled;
