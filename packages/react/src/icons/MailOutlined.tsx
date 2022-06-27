63002afae4fe0e69134cb3d3f0f037cfade470a0
wangxingkang <wang_xingkang@qq.com>
2022-06-27 18:19:11 +0800

    feat: add icons

---
+++ packages/react/src/icons/MailOutlined.tsx
@@ -0,0 +1,12 @@
+// This icon file is generated automatically.
+import * as React from 'react';
+import MailOutlinedSvg from '@sensoro-design/icons-svg/lib/asn/MailOutlined';
+import SensoroIcon, { SensoroIconProps } from '../components/SensoroIcon';
+
+const MailOutlined = React.forwardRef<HTMLSpanElement, SensoroIconProps>((props, ref) => (
+  <SensoroIcon {...props} ref={ref} icon={MailOutlinedSvg} />
+));
+
+MailOutlined.displayName = 'MailOutlined';
+
+export default MailOutlined;
---
+++ packages/react/src/icons/PhoneOutlined.tsx
@@ -0,0 +1,12 @@
+// This icon file is generated automatically.
+import * as React from 'react';
+import PhoneOutlinedSvg from '@sensoro-design/icons-svg/lib/asn/PhoneOutlined';
+import SensoroIcon, { SensoroIconProps } from '../components/SensoroIcon';
+
+const PhoneOutlined = React.forwardRef<HTMLSpanElement, SensoroIconProps>((props, ref) => (
+  <SensoroIcon {...props} ref={ref} icon={PhoneOutlinedSvg} />
+));
+
+PhoneOutlined.displayName = 'PhoneOutlined';
+
+export default PhoneOutlined;
