4d1d84089ec8a2b349264ce3d98f0dde0924dfa7
wangxingkang <wang_xingkang@qq.com>
2022-07-14 15:59:24 +0800

    feat: add icon

---
+++ packages/react/src/icons/IllegallyBuiltOutlined.tsx
@@ -0,0 +1,12 @@
+// This icon file is generated automatically.
+import * as React from 'react';
+import IllegallyBuiltOutlinedSvg from '@sensoro-design/icons-svg/lib/asn/IllegallyBuiltOutlined';
+import SensoroIcon, { SensoroIconProps } from '../components/SensoroIcon';
+
+const IllegallyBuiltOutlined = React.forwardRef<HTMLSpanElement, SensoroIconProps>((props, ref) => (
+  <SensoroIcon {...props} ref={ref} icon={IllegallyBuiltOutlinedSvg} />
+));
+
+IllegallyBuiltOutlined.displayName = 'IllegallyBuiltOutlined';
+
+export default IllegallyBuiltOutlined;
