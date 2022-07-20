98e076fb486e683cb0b7fa2373b6bdcfe0ac65b8
wangxingkang <wang_xingkang@qq.com>
2022-07-20 23:17:45 +0800

    docs: test

--- docs/components/IconDisplay/fields.ts
+++ docs/components/IconDisplay/fields.ts
@@ -34,7 +34,7 @@ const direction: string[] = [
   'RightDown',
 ];
 
-const car = ['Car', 'CarCamera', 'CarRecord', 'CarUnknown', 'CarExclamationCircle'];
+const car = ['Car', 'CarCamera', 'CarRecord', 'CarUnknown', 'CarExclamationCircle', 'CarPen', 'CarQuestionCircle'];
 
 const built = ['Building', 'Employer', 'IllegalBuilding'];
 
