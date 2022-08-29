ffdb9d3299ede1a8112bddce3969fa6ebcec43b7
wangxingkang <wang_xingkang@qq.com>
2022-08-29 16:02:26 +0800

    feat: add icons

---
+++ packages/react/src/icons/SunOutlined.tsx
@@ -0,0 +1,12 @@
+// This icon file is generated automatically.
+import * as React from 'react';
+import SunOutlinedSvg from '@sensoro-design/icons-svg/lib/asn/SunOutlined';
+import SensoroIcon, { SensoroIconProps } from '../components/SensoroIcon';
+
+const SunOutlined = React.forwardRef<HTMLSpanElement, SensoroIconProps>((props, ref) => (
+  <SensoroIcon {...props} ref={ref} icon={SunOutlinedSvg} />
+));
+
+SunOutlined.displayName = 'SunOutlined';
+
+export default SunOutlined;
