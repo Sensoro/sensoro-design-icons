19d725d800ce851ce47e444e4699c7aab29ce6f4
wangxingkang <wang_xingkang@qq.com>
2022-10-10 18:17:50 +0800

    feat: add icons

---
+++ packages/react/src/icons/BuildingPlusCircleOutlined.tsx
@@ -0,0 +1,12 @@
+// This icon file is generated automatically.
+import * as React from 'react';
+import BuildingPlusCircleOutlinedSvg from '@sensoro-design/icons-svg/lib/asn/BuildingPlusCircleOutlined';
+import SensoroIcon, { SensoroIconProps } from '../components/SensoroIcon';
+
+const BuildingPlusCircleOutlined = React.forwardRef<HTMLSpanElement, SensoroIconProps>((props, ref) => (
+  <SensoroIcon {...props} ref={ref} icon={BuildingPlusCircleOutlinedSvg} />
+));
+
+BuildingPlusCircleOutlined.displayName = 'BuildingPlusCircleOutlined';
+
+export default BuildingPlusCircleOutlined;
---
+++ packages/react/src/icons/SafetyKeyOutlined.tsx
@@ -0,0 +1,12 @@
+// This icon file is generated automatically.
+import * as React from 'react';
+import SafetyKeyOutlinedSvg from '@sensoro-design/icons-svg/lib/asn/SafetyKeyOutlined';
+import SensoroIcon, { SensoroIconProps } from '../components/SensoroIcon';
+
+const SafetyKeyOutlined = React.forwardRef<HTMLSpanElement, SensoroIconProps>((props, ref) => (
+  <SensoroIcon {...props} ref={ref} icon={SafetyKeyOutlinedSvg} />
+));
+
+SafetyKeyOutlined.displayName = 'SafetyKeyOutlined';
+
+export default SafetyKeyOutlined;
