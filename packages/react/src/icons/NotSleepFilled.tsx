5cb6fa843f9e1e8bd5ec83ed0a0642ec8e8981c0
wangxingkang <wang_xingkang@qq.com>
2022-10-17 23:14:31 +0800

    feat: add  icons

---
+++ packages/react/src/icons/NotSleepFilled.tsx
@@ -0,0 +1,12 @@
+// This icon file is generated automatically.
+import * as React from 'react';
+import NotSleepFilledSvg from '@sensoro-design/icons-svg/lib/asn/NotSleepFilled';
+import SensoroIcon, { SensoroIconProps } from '../components/SensoroIcon';
+
+const NotSleepFilled = React.forwardRef<HTMLSpanElement, SensoroIconProps>((props, ref) => (
+  <SensoroIcon {...props} ref={ref} icon={NotSleepFilledSvg} />
+));
+
+NotSleepFilled.displayName = 'NotSleepFilled';
+
+export default NotSleepFilled;
---
+++ packages/react/src/icons/SleepFilled.tsx
@@ -0,0 +1,12 @@
+// This icon file is generated automatically.
+import * as React from 'react';
+import SleepFilledSvg from '@sensoro-design/icons-svg/lib/asn/SleepFilled';
+import SensoroIcon, { SensoroIconProps } from '../components/SensoroIcon';
+
+const SleepFilled = React.forwardRef<HTMLSpanElement, SensoroIconProps>((props, ref) => (
+  <SensoroIcon {...props} ref={ref} icon={SleepFilledSvg} />
+));
+
+SleepFilled.displayName = 'SleepFilled';
+
+export default SleepFilled;
