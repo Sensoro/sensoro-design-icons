ba3375971b90ed3a34df1b4ebb3912b95fe8fae3
wangxingkang <wang_xingkang@qq.com>
2022-10-21 23:44:19 +0800

    feat: add icons

---
+++ packages/react/src/icons/CycleOutlined.tsx
@@ -0,0 +1,12 @@
+// This icon file is generated automatically.
+import * as React from 'react';
+import CycleOutlinedSvg from '@sensoro-design/icons-svg/lib/asn/CycleOutlined';
+import SensoroIcon, { SensoroIconProps } from '../components/SensoroIcon';
+
+const CycleOutlined = React.forwardRef<HTMLSpanElement, SensoroIconProps>((props, ref) => (
+  <SensoroIcon {...props} ref={ref} icon={CycleOutlinedSvg} />
+));
+
+CycleOutlined.displayName = 'CycleOutlined';
+
+export default CycleOutlined;
---
+++ packages/react/src/icons/UserPurely.tsx
@@ -0,0 +1,12 @@
+// This icon file is generated automatically.
+import * as React from 'react';
+import UserPurelySvg from '@sensoro-design/icons-svg/lib/asn/UserPurely';
+import SensoroIcon, { SensoroIconProps } from '../components/SensoroIcon';
+
+const UserPurely = React.forwardRef<HTMLSpanElement, SensoroIconProps>((props, ref) => (
+  <SensoroIcon {...props} ref={ref} icon={UserPurelySvg} />
+));
+
+UserPurely.displayName = 'UserPurely';
+
+export default UserPurely;
