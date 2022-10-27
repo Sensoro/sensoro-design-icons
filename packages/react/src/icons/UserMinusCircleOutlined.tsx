3112fc740a9df3eea41798060b9c85fd19e0dc1a
wangxingkang <wang_xingkang@qq.com>
2022-10-27 11:36:41 +0800

    feat: add icons

---
+++ packages/react/src/icons/UserMinusCircleOutlined.tsx
@@ -0,0 +1,12 @@
+// This icon file is generated automatically.
+import * as React from 'react';
+import UserMinusCircleOutlinedSvg from '@sensoro-design/icons-svg/lib/asn/UserMinusCircleOutlined';
+import SensoroIcon, { SensoroIconProps } from '../components/SensoroIcon';
+
+const UserMinusCircleOutlined = React.forwardRef<HTMLSpanElement, SensoroIconProps>((props, ref) => (
+  <SensoroIcon {...props} ref={ref} icon={UserMinusCircleOutlinedSvg} />
+));
+
+UserMinusCircleOutlined.displayName = 'UserMinusCircleOutlined';
+
+export default UserMinusCircleOutlined;
---
+++ packages/react/src/icons/UserTimeOutlined.tsx
@@ -0,0 +1,12 @@
+// This icon file is generated automatically.
+import * as React from 'react';
+import UserTimeOutlinedSvg from '@sensoro-design/icons-svg/lib/asn/UserTimeOutlined';
+import SensoroIcon, { SensoroIconProps } from '../components/SensoroIcon';
+
+const UserTimeOutlined = React.forwardRef<HTMLSpanElement, SensoroIconProps>((props, ref) => (
+  <SensoroIcon {...props} ref={ref} icon={UserTimeOutlinedSvg} />
+));
+
+UserTimeOutlined.displayName = 'UserTimeOutlined';
+
+export default UserTimeOutlined;
