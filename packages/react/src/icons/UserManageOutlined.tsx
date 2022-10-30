1297ab9799f91e7ef6d7241ea0430334ae2c310b
wangxingkang <wang_xingkang@qq.com>
2022-10-30 17:01:01 +0800

    feat: add UserMange、UserSetting

---
+++ packages/react/src/icons/UserManageOutlined.tsx
@@ -0,0 +1,12 @@
+// This icon file is generated automatically.
+import * as React from 'react';
+import UserManageOutlinedSvg from '@sensoro-design/icons-svg/lib/asn/UserManageOutlined';
+import SensoroIcon, { SensoroIconProps } from '../components/SensoroIcon';
+
+const UserManageOutlined = React.forwardRef<HTMLSpanElement, SensoroIconProps>((props, ref) => (
+  <SensoroIcon {...props} ref={ref} icon={UserManageOutlinedSvg} />
+));
+
+UserManageOutlined.displayName = 'UserManageOutlined';
+
+export default UserManageOutlined;
---
+++ packages/react/src/icons/UserSettingOutlined.tsx
@@ -0,0 +1,12 @@
+// This icon file is generated automatically.
+import * as React from 'react';
+import UserSettingOutlinedSvg from '@sensoro-design/icons-svg/lib/asn/UserSettingOutlined';
+import SensoroIcon, { SensoroIconProps } from '../components/SensoroIcon';
+
+const UserSettingOutlined = React.forwardRef<HTMLSpanElement, SensoroIconProps>((props, ref) => (
+  <SensoroIcon {...props} ref={ref} icon={UserSettingOutlinedSvg} />
+));
+
+UserSettingOutlined.displayName = 'UserSettingOutlined';
+
+export default UserSettingOutlined;
