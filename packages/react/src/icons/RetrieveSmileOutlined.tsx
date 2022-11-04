200fd8e3808fca8686eeba9a3df79ed225dbdb82
wangxingkang <wang_xingkang@qq.com>
2022-11-04 16:55:19 +0800

    feat: 补充提交

---
+++ packages/react/src/icons/RetrieveSmileOutlined.tsx
@@ -0,0 +1,12 @@
+// This icon file is generated automatically.
+import * as React from 'react';
+import RetrieveSmileOutlinedSvg from '@sensoro-design/icons-svg/lib/asn/RetrieveSmileOutlined';
+import SensoroIcon, { SensoroIconProps } from '../components/SensoroIcon';
+
+const RetrieveSmileOutlined = React.forwardRef<HTMLSpanElement, SensoroIconProps>((props, ref) => (
+  <SensoroIcon {...props} ref={ref} icon={RetrieveSmileOutlinedSvg} />
+));
+
+RetrieveSmileOutlined.displayName = 'RetrieveSmileOutlined';
+
+export default RetrieveSmileOutlined;
