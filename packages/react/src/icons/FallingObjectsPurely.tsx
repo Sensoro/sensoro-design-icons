497210346e004af4f3b1cf918f9db59d25d01f93
wangxingkang <wang_xingkang@qq.com>
2022-11-04 16:51:26 +0800

    feat: add icons

---
+++ packages/react/src/icons/FallingObjectsPurely.tsx
@@ -0,0 +1,12 @@
+// This icon file is generated automatically.
+import * as React from 'react';
+import FallingObjectsPurelySvg from '@sensoro-design/icons-svg/lib/asn/FallingObjectsPurely';
+import SensoroIcon, { SensoroIconProps } from '../components/SensoroIcon';
+
+const FallingObjectsPurely = React.forwardRef<HTMLSpanElement, SensoroIconProps>((props, ref) => (
+  <SensoroIcon {...props} ref={ref} icon={FallingObjectsPurelySvg} />
+));
+
+FallingObjectsPurely.displayName = 'FallingObjectsPurely';
+
+export default FallingObjectsPurely;
---
+++ packages/react/src/icons/NucleicAcidOutlined.tsx
@@ -0,0 +1,12 @@
+// This icon file is generated automatically.
+import * as React from 'react';
+import NucleicAcidOutlinedSvg from '@sensoro-design/icons-svg/lib/asn/NucleicAcidOutlined';
+import SensoroIcon, { SensoroIconProps } from '../components/SensoroIcon';
+
+const NucleicAcidOutlined = React.forwardRef<HTMLSpanElement, SensoroIconProps>((props, ref) => (
+  <SensoroIcon {...props} ref={ref} icon={NucleicAcidOutlinedSvg} />
+));
+
+NucleicAcidOutlined.displayName = 'NucleicAcidOutlined';
+
+export default NucleicAcidOutlined;
