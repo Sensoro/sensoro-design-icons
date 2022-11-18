519b43ac0cef992f4b2dd041d859d7b2d75673e7
wangxingkang <wang_xingkang@qq.com>
2022-11-18 17:53:29 +0800

    feat: add icons

---
+++ packages/react/src/icons/UserMinusCircleFilled.tsx
@@ -0,0 +1,12 @@
+// This icon file is generated automatically.
+import * as React from 'react';
+import UserMinusCircleFilledSvg from '@sensoro-design/icons-svg/lib/asn/UserMinusCircleFilled';
+import SensoroIcon, { SensoroIconProps } from '../components/SensoroIcon';
+
+const UserMinusCircleFilled = React.forwardRef<HTMLSpanElement, SensoroIconProps>((props, ref) => (
+  <SensoroIcon {...props} ref={ref} icon={UserMinusCircleFilledSvg} />
+));
+
+UserMinusCircleFilled.displayName = 'UserMinusCircleFilled';
+
+export default UserMinusCircleFilled;
---
+++ packages/react/src/icons/UserPlusCircleFilled.tsx
@@ -0,0 +1,12 @@
+// This icon file is generated automatically.
+import * as React from 'react';
+import UserPlusCircleFilledSvg from '@sensoro-design/icons-svg/lib/asn/UserPlusCircleFilled';
+import SensoroIcon, { SensoroIconProps } from '../components/SensoroIcon';
+
+const UserPlusCircleFilled = React.forwardRef<HTMLSpanElement, SensoroIconProps>((props, ref) => (
+  <SensoroIcon {...props} ref={ref} icon={UserPlusCircleFilledSvg} />
+));
+
+UserPlusCircleFilled.displayName = 'UserPlusCircleFilled';
+
+export default UserPlusCircleFilled;
