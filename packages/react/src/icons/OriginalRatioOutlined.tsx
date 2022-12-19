adae301c75f7d1ef2b8d7f62dfc2125b9d0a4d31
wangxingkang <wang_xingkang@qq.com>
2022-12-19 19:01:24 +0800

    feat: 修改分类

---
+++ packages/react/src/icons/OriginalRatioOutlined.tsx
@@ -0,0 +1,12 @@
+// This icon file is generated automatically.
+import * as React from 'react';
+import OriginalRatioOutlinedSvg from '@sensoro-design/icons-svg/es/asn/OriginalRatioOutlined';
+import SensoroIcon, { SensoroIconProps } from '../components/SensoroIcon';
+
+const OriginalRatioOutlined = React.forwardRef<HTMLSpanElement, SensoroIconProps>((props, ref) => (
+  <SensoroIcon {...props} ref={ref} icon={OriginalRatioOutlinedSvg} />
+));
+
+OriginalRatioOutlined.displayName = 'OriginalRatioOutlined';
+
+export default OriginalRatioOutlined;
