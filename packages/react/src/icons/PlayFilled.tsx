275ef8933357eadf7c380cd3c10da13eee541895
wangxingkang <wang_xingkang@qq.com>
2023-01-04 19:57:33 +0800

    feat: add ScreenshotOutlined

---
+++ packages/react/src/icons/PlayFilled.tsx
@@ -0,0 +1,12 @@
+// This icon file is generated automatically.
+import * as React from 'react';
+import PlayFilledSvg from '@sensoro-design/icons-svg/es/asn/PlayFilled';
+import SensoroIcon, { SensoroIconProps } from '../components/SensoroIcon';
+
+const PlayFilled = React.forwardRef<HTMLSpanElement, SensoroIconProps>((props, ref) => (
+  <SensoroIcon {...props} ref={ref} icon={PlayFilledSvg} />
+));
+
+PlayFilled.displayName = 'PlayFilled';
+
+export default PlayFilled;
---
+++ packages/react/src/icons/ScreenshotOutlined.tsx
@@ -0,0 +1,12 @@
+// This icon file is generated automatically.
+import * as React from 'react';
+import ScreenshotOutlinedSvg from '@sensoro-design/icons-svg/es/asn/ScreenshotOutlined';
+import SensoroIcon, { SensoroIconProps } from '../components/SensoroIcon';
+
+const ScreenshotOutlined = React.forwardRef<HTMLSpanElement, SensoroIconProps>((props, ref) => (
+  <SensoroIcon {...props} ref={ref} icon={ScreenshotOutlinedSvg} />
+));
+
+ScreenshotOutlined.displayName = 'ScreenshotOutlined';
+
+export default ScreenshotOutlined;
