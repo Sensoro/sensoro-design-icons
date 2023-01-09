82812e9588755151c1939862c2d2351f7adeeacc
wangxingkang <wang_xingkang@qq.com>
2023-01-09 11:10:37 +0800

    feat: 播放图标修改

---
+++ packages/react/src/icons/PlayCircleProOutlined.tsx
@@ -0,0 +1,12 @@
+// This icon file is generated automatically.
+import * as React from 'react';
+import PlayCircleProOutlinedSvg from '@sensoro-design/icons-svg/es/asn/PlayCircleProOutlined';
+import SensoroIcon, { SensoroIconProps } from '../components/SensoroIcon';
+
+const PlayCircleProOutlined = React.forwardRef<HTMLSpanElement, SensoroIconProps>((props, ref) => (
+  <SensoroIcon {...props} ref={ref} icon={PlayCircleProOutlinedSvg} />
+));
+
+PlayCircleProOutlined.displayName = 'PlayCircleProOutlined';
+
+export default PlayCircleProOutlined;
