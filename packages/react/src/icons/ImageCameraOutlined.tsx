75869041ee5847ade1edb6f6ac9239397fea2721
wangxingkang <wang_xingkang@qq.com>
2023-02-01 13:05:12 +0800

    feat: add icons

---
+++ packages/react/src/icons/ImageCameraOutlined.tsx
@@ -0,0 +1,12 @@
+// This icon file is generated automatically.
+import * as React from 'react';
+import ImageCameraOutlinedSvg from '@sensoro-design/icons-svg/es/asn/ImageCameraOutlined';
+import SensoroIcon, { SensoroIconProps } from '../components/SensoroIcon';
+
+const ImageCameraOutlined = React.forwardRef<HTMLSpanElement, SensoroIconProps>((props, ref) => (
+  <SensoroIcon {...props} ref={ref} icon={ImageCameraOutlinedSvg} />
+));
+
+ImageCameraOutlined.displayName = 'ImageCameraOutlined';
+
+export default ImageCameraOutlined;
