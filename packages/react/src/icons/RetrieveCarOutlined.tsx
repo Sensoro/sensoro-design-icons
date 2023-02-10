259d3692ec956ec845c9b4b29a771db16e947bc8
wangxingkang <wang_xingkang@qq.com>
2023-02-10 11:33:10 +0800

    feat: add RetrieveCarOutlined RetrieveHumanoidOutlined

---
+++ packages/react/src/icons/RetrieveCarOutlined.tsx
@@ -0,0 +1,12 @@
+// This icon file is generated automatically.
+import * as React from 'react';
+import RetrieveCarOutlinedSvg from '@sensoro-design/icons-svg/es/asn/RetrieveCarOutlined';
+import SensoroIcon, { SensoroIconProps } from '../components/SensoroIcon';
+
+const RetrieveCarOutlined = React.forwardRef<HTMLSpanElement, SensoroIconProps>((props, ref) => (
+  <SensoroIcon {...props} ref={ref} icon={RetrieveCarOutlinedSvg} />
+));
+
+RetrieveCarOutlined.displayName = 'RetrieveCarOutlined';
+
+export default RetrieveCarOutlined;
---
+++ packages/react/src/icons/RetrieveHumanoidOutlined.tsx
@@ -0,0 +1,12 @@
+// This icon file is generated automatically.
+import * as React from 'react';
+import RetrieveHumanoidOutlinedSvg from '@sensoro-design/icons-svg/es/asn/RetrieveHumanoidOutlined';
+import SensoroIcon, { SensoroIconProps } from '../components/SensoroIcon';
+
+const RetrieveHumanoidOutlined = React.forwardRef<HTMLSpanElement, SensoroIconProps>((props, ref) => (
+  <SensoroIcon {...props} ref={ref} icon={RetrieveHumanoidOutlinedSvg} />
+));
+
+RetrieveHumanoidOutlined.displayName = 'RetrieveHumanoidOutlined';
+
+export default RetrieveHumanoidOutlined;
