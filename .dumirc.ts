01dbb56ccb2dd5fb33b9fa1b1aadc2c5f0028498
wangxingkang <wang_xingkang@qq.com>
2023-02-10 11:56:22 +0800

    chore: 修复热更新失效

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,3 +1,4 @@
+import path from 'path';
 import { defineConfig } from 'dumi';
 
 const logo = 'https://cdn.jsdelivr.net/gh/wangxingkang/pictures@latest/imgs/sensoro-design.svg';
@@ -9,6 +10,12 @@ export default defineConfig({
     socialLinks: {
       github: 'https://github.com/sensoro-design/sensoro-design-icons',
     },
+  },
+  alias: {
+    '@sensoro-design/icons-svg/es': path.resolve(__dirname, './packages/core/src'),
+    '@sensoro-design/icons-svg': path.resolve(__dirname, './packages/core/src/index.ts'),
+    '@sensoro-design/icons/es': path.resolve(__dirname, './packages/react/src'),
+    '@sensoro-design/icons': path.resolve(__dirname, './packages/react/src/index.ts'),
   },
   favicons: [logo],
   hash: true,
