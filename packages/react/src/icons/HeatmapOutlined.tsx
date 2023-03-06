1c33494d05dbf018d98edcb09d5136ac74da6c17
wangxingkang <wang_xingkang@qq.com>
2023-03-06 14:52:30 +0800

    chore: add HeatmapOutlined

---
+++ packages/react/src/icons/HeatmapOutlined.tsx
@@ -0,0 +1,12 @@
+// This icon file is generated automatically.
+import * as React from 'react';
+import HeatmapOutlinedSvg from '@sensoro-design/icons-svg/es/asn/HeatmapOutlined';
+import SensoroIcon, { SensoroIconProps } from '../components/SensoroIcon';
+
+const HeatmapOutlined = React.forwardRef<HTMLSpanElement, SensoroIconProps>((props, ref) => (
+  <SensoroIcon {...props} ref={ref} icon={HeatmapOutlinedSvg} />
+));
+
+HeatmapOutlined.displayName = 'HeatmapOutlined';
+
+export default HeatmapOutlined;
