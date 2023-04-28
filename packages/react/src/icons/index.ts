adf35ce5114f7fcaf42237e13db380b0fb961663
wangxingkang <wang_xingkang@qq.com>
2023-04-28 12:08:15 +0800

    feat: add TaskOutlined

--- packages/react/src/icons/index.ts
+++ packages/react/src/icons/index.ts
@@ -673,6 +673,7 @@ export { default as SyncOutlined } from './SyncOutlined';
 export { default as TagFilled } from './TagFilled';
 export { default as TagOutlined } from './TagOutlined';
 export { default as TakeOffOutlined } from './TakeOffOutlined';
+export { default as TaskOutlined } from './TaskOutlined';
 export { default as TemperaturePatchOutlined } from './TemperaturePatchOutlined';
 export { default as TemplateFilled } from './TemplateFilled';
 export { default as TenantFilled } from './TenantFilled';
