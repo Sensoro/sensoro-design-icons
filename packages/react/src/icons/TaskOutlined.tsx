9137c16acb039d68e092a665f3dba86684ad45b5
wangxingkang <wang_xingkang@qq.com>
2023-04-28 12:08:48 +0800

    feat: add TaskOutlined

---
+++ packages/react/src/icons/TaskOutlined.tsx
@@ -0,0 +1,12 @@
+// This icon file is generated automatically.
+import * as React from 'react';
+import TaskOutlinedSvg from '@sensoro-design/icons-svg/es/asn/TaskOutlined';
+import SensoroIcon, { SensoroIconProps } from '../components/SensoroIcon';
+
+const TaskOutlined = React.forwardRef<HTMLSpanElement, SensoroIconProps>((props, ref) => (
+  <SensoroIcon {...props} ref={ref} icon={TaskOutlinedSvg} />
+));
+
+TaskOutlined.displayName = 'TaskOutlined';
+
+export default TaskOutlined;
