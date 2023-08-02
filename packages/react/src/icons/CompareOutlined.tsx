9e55001aeec07c2dfda6488ee0add362c0de3048
wangxingkang <wang_xingkang@qq.com>
2023-08-02 16:14:14 +0800

    feat: add CompareOutlined、TimeMachineOutlined

---
+++ packages/react/src/icons/CompareOutlined.tsx
@@ -0,0 +1,12 @@
+// This icon file is generated automatically.
+import * as React from 'react';
+import CompareOutlinedSvg from '@sensoro-design/icons-svg/es/asn/CompareOutlined';
+import SensoroIcon, { SensoroIconProps } from '../components/SensoroIcon';
+
+const CompareOutlined = React.forwardRef<HTMLSpanElement, SensoroIconProps>((props, ref) => (
+  <SensoroIcon {...props} ref={ref} icon={CompareOutlinedSvg} />
+));
+
+CompareOutlined.displayName = 'CompareOutlined';
+
+export default CompareOutlined;
---
+++ packages/react/src/icons/TimeMachineOutlined.tsx
@@ -0,0 +1,12 @@
+// This icon file is generated automatically.
+import * as React from 'react';
+import TimeMachineOutlinedSvg from '@sensoro-design/icons-svg/es/asn/TimeMachineOutlined';
+import SensoroIcon, { SensoroIconProps } from '../components/SensoroIcon';
+
+const TimeMachineOutlined = React.forwardRef<HTMLSpanElement, SensoroIconProps>((props, ref) => (
+  <SensoroIcon {...props} ref={ref} icon={TimeMachineOutlinedSvg} />
+));
+
+TimeMachineOutlined.displayName = 'TimeMachineOutlined';
+
+export default TimeMachineOutlined;
