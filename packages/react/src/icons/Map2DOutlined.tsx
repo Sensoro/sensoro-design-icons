ef1b338d36340ee2fe4e63b1aec31be138567df8
wangxingkang <wang_xingkang@qq.com>
2023-08-15 10:51:33 +0800

    feat: add Map2DOutlined Map3DOutlined

---
+++ packages/react/src/icons/Map2DOutlined.tsx
@@ -0,0 +1,12 @@
+// This icon file is generated automatically.
+import * as React from 'react';
+import Map2DOutlinedSvg from '@sensoro-design/icons-svg/es/asn/Map2DOutlined';
+import SensoroIcon, { SensoroIconProps } from '../components/SensoroIcon';
+
+const Map2DOutlined = React.forwardRef<HTMLSpanElement, SensoroIconProps>((props, ref) => (
+  <SensoroIcon {...props} ref={ref} icon={Map2DOutlinedSvg} />
+));
+
+Map2DOutlined.displayName = 'Map2DOutlined';
+
+export default Map2DOutlined;
--- packages/react/src/icons/index.ts
+++ packages/react/src/icons/index.ts
@@ -429,6 +429,8 @@ export { default as MaleOutlined } from './MaleOutlined';
 export { default as ManholeCoverOutlined } from './ManholeCoverOutlined';
 export { default as ManholeCoverPurely } from './ManholeCoverPurely';
 export { default as ManualAlarmCircleOutlined } from './ManualAlarmCircleOutlined';
+export { default as Map2DOutlined } from './Map2DOutlined';
+export { default as Map3DOutlined } from './Map3DOutlined';
 export { default as MeansAccessOutlined } from './MeansAccessOutlined';
 export { default as MedicalFilled } from './MedicalFilled';
 export { default as MedicalKitOutlined } from './MedicalKitOutlined';
