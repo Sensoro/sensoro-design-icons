ac4bc1b4de38fe8c58901561bc95361e51a3cb9c
wangxingkang <wang_xingkang@qq.com>
2023-09-18 19:42:56 +0800

    feat: add CaseAssistanceOutlined、VolunteerServiceOutlined

---
+++ packages/react/src/icons/CaseAssistanceOutlined.tsx
@@ -0,0 +1,12 @@
+// This icon file is generated automatically.
+import * as React from 'react';
+import CaseAssistanceOutlinedSvg from '@sensoro-design/icons-svg/es/asn/CaseAssistanceOutlined';
+import SensoroIcon, { SensoroIconProps } from '../components/SensoroIcon';
+
+const CaseAssistanceOutlined = React.forwardRef<HTMLSpanElement, SensoroIconProps>((props, ref) => (
+  <SensoroIcon {...props} ref={ref} icon={CaseAssistanceOutlinedSvg} />
+));
+
+CaseAssistanceOutlined.displayName = 'CaseAssistanceOutlined';
+
+export default CaseAssistanceOutlined;
---
+++ packages/react/src/icons/VolunteerServiceOutlined.tsx
@@ -0,0 +1,12 @@
+// This icon file is generated automatically.
+import * as React from 'react';
+import VolunteerServiceOutlinedSvg from '@sensoro-design/icons-svg/es/asn/VolunteerServiceOutlined';
+import SensoroIcon, { SensoroIconProps } from '../components/SensoroIcon';
+
+const VolunteerServiceOutlined = React.forwardRef<HTMLSpanElement, SensoroIconProps>((props, ref) => (
+  <SensoroIcon {...props} ref={ref} icon={VolunteerServiceOutlinedSvg} />
+));
+
+VolunteerServiceOutlined.displayName = 'VolunteerServiceOutlined';
+
+export default VolunteerServiceOutlined;
