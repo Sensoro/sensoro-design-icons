989955e2bbf2667acb94a3fc93577a0509dab29e
wangxingkang <wang_xingkang@qq.com>
2023-09-22 15:40:42 +0800

    feat: add AttendanceRateFilled UserCloseCircleFilled

---
+++ packages/react/src/icons/AttendanceRateFilled.tsx
@@ -0,0 +1,12 @@
+// This icon file is generated automatically.
+import * as React from 'react';
+import AttendanceRateFilledSvg from '@sensoro-design/icons-svg/es/asn/AttendanceRateFilled';
+import SensoroIcon, { SensoroIconProps } from '../components/SensoroIcon';
+
+const AttendanceRateFilled = React.forwardRef<HTMLSpanElement, SensoroIconProps>((props, ref) => (
+  <SensoroIcon {...props} ref={ref} icon={AttendanceRateFilledSvg} />
+));
+
+AttendanceRateFilled.displayName = 'AttendanceRateFilled';
+
+export default AttendanceRateFilled;
---
+++ packages/react/src/icons/UserCloseCircleFilled.tsx
@@ -0,0 +1,12 @@
+// This icon file is generated automatically.
+import * as React from 'react';
+import UserCloseCircleFilledSvg from '@sensoro-design/icons-svg/es/asn/UserCloseCircleFilled';
+import SensoroIcon, { SensoroIconProps } from '../components/SensoroIcon';
+
+const UserCloseCircleFilled = React.forwardRef<HTMLSpanElement, SensoroIconProps>((props, ref) => (
+  <SensoroIcon {...props} ref={ref} icon={UserCloseCircleFilledSvg} />
+));
+
+UserCloseCircleFilled.displayName = 'UserCloseCircleFilled';
+
+export default UserCloseCircleFilled;
