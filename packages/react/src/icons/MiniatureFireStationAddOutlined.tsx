4c820578c4cee3ee7d26ca90791f0ff486b3bbab
wangxingkang <wang_xingkang@qq.com>
2023-10-30 18:30:55 +0800

    feat: add MiniatureFireStationAddOutlined

---
+++ packages/react/src/icons/MiniatureFireStationAddOutlined.tsx
@@ -0,0 +1,12 @@
+// This icon file is generated automatically.
+import * as React from 'react';
+import MiniatureFireStationAddOutlinedSvg from '@sensoro-design/icons-svg/es/asn/MiniatureFireStationAddOutlined';
+import SensoroIcon, { SensoroIconProps } from '../components/SensoroIcon';
+
+const MiniatureFireStationAddOutlined = React.forwardRef<HTMLSpanElement, SensoroIconProps>((props, ref) => (
+  <SensoroIcon {...props} ref={ref} icon={MiniatureFireStationAddOutlinedSvg} />
+));
+
+MiniatureFireStationAddOutlined.displayName = 'MiniatureFireStationAddOutlined';
+
+export default MiniatureFireStationAddOutlined;
