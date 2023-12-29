5b3ee14f5b215941b7256b7b1f081db43f5dcb85
wangxingkang <wang_xingkang@qq.com>
2023-12-29 19:03:56 +0800

    chore: 完善示例

--- examples/vite-react/vite.config.ts
+++ examples/vite-react/vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite';
+import { defineConfig, type PluginOption } from 'vite';
+import { visualizer } from 'rollup-plugin-visualizer';
 import react from '@vitejs/plugin-react';
+
+const analyze = process.env.ANALYZE;
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    analyze &&
+      (visualizer({
+        gzipSize: true,
+        brotliSize: true,
+        emitFile: false,
+        open: true,
+      }) as unknown as PluginOption),
+  ],
 });
