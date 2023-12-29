cb46babceed914929b22f61d1100aa067de8ab8d
wangxingkang <wang_xingkang@qq.com>
2023-12-29 16:51:33 +0800

    feat: add icons

---
+++ packages/react/src/icons/BurningPointOutlined.tsx
@@ -0,0 +1,12 @@
+// This icon file is generated automatically.
+import * as React from 'react';
+import BurningPointOutlinedSvg from '@sensoro-design/icons-svg/es/asn/BurningPointOutlined';
+import SensoroIcon, { SensoroIconProps } from '../components/SensoroIcon';
+
+const BurningPointOutlined = React.forwardRef<HTMLSpanElement, SensoroIconProps>((props, ref) => (
+  <SensoroIcon {...props} ref={ref} icon={BurningPointOutlinedSvg} />
+));
+
+BurningPointOutlined.displayName = 'BurningPointOutlined';
+
+export default BurningPointOutlined;
---
+++ packages/react/src/icons/SafetyFireOutlined.tsx
@@ -0,0 +1,12 @@
+// This icon file is generated automatically.
+import * as React from 'react';
+import SafetyFireOutlinedSvg from '@sensoro-design/icons-svg/es/asn/SafetyFireOutlined';
+import SensoroIcon, { SensoroIconProps } from '../components/SensoroIcon';
+
+const SafetyFireOutlined = React.forwardRef<HTMLSpanElement, SensoroIconProps>((props, ref) => (
+  <SensoroIcon {...props} ref={ref} icon={SafetyFireOutlinedSvg} />
+));
+
+SafetyFireOutlined.displayName = 'SafetyFireOutlined';
+
+export default SafetyFireOutlined;
