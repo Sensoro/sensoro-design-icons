a366dc765476b2ba5cc0c583cbba487729b859b0
wangxingkang <wang_xingkang@qq.com>
2023-12-29 15:02:57 +0800

    feat: rename AudioMuteFilled

---
+++ packages/react/src/icons/AudioMutedFilled.tsx
@@ -0,0 +1,12 @@
+// This icon file is generated automatically.
+import * as React from 'react';
+import AudioMutedFilledSvg from '@sensoro-design/icons-svg/es/asn/AudioMutedFilled';
+import SensoroIcon, { SensoroIconProps } from '../components/SensoroIcon';
+
+const AudioMutedFilled = React.forwardRef<HTMLSpanElement, SensoroIconProps>((props, ref) => (
+  <SensoroIcon {...props} ref={ref} icon={AudioMutedFilledSvg} />
+));
+
+AudioMutedFilled.displayName = 'AudioMutedFilled';
+
+export default AudioMutedFilled;
