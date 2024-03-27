7880e5e20a4a3562e25e49e9dd9c005b4cc05ffc
wangxingkang <wang_xingkang@qq.com>
2024-03-27 11:27:55 +0800

    feat: add AvatarPurely、ImagePurely

---
+++ packages/react/src/icons/AvatarPurely.tsx
@@ -0,0 +1,12 @@
+// This icon file is generated automatically.
+import * as React from 'react';
+import AvatarPurelySvg from '@sensoro-design/icons-svg/es/asn/AvatarPurely';
+import SensoroIcon, { SensoroIconProps } from '../components/SensoroIcon';
+
+const AvatarPurely = React.forwardRef<HTMLSpanElement, SensoroIconProps>((props, ref) => (
+  <SensoroIcon {...props} ref={ref} icon={AvatarPurelySvg} />
+));
+
+AvatarPurely.displayName = 'AvatarPurely';
+
+export default AvatarPurely;
---
+++ packages/react/src/icons/ImagePurely.tsx
@@ -0,0 +1,12 @@
+// This icon file is generated automatically.
+import * as React from 'react';
+import ImagePurelySvg from '@sensoro-design/icons-svg/es/asn/ImagePurely';
+import SensoroIcon, { SensoroIconProps } from '../components/SensoroIcon';
+
+const ImagePurely = React.forwardRef<HTMLSpanElement, SensoroIconProps>((props, ref) => (
+  <SensoroIcon {...props} ref={ref} icon={ImagePurelySvg} />
+));
+
+ImagePurely.displayName = 'ImagePurely';
+
+export default ImagePurely;
